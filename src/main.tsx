f6554baed8000c8a4e765d3d56c28720314e3ebb
Bishop <jgbispo20@gmail.com>
2022-08-24 15:57:59 -0300

    remove import not used

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import theme from './theme'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
