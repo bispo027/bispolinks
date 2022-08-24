0114e89d4f07bd3f88b0c3a8a0a097240976e7a0
Bishop <jgbispo20@gmail.com>
2022-08-24 16:09:13 -0300

    alter links

--- src/components/ListLinks.tsx
+++ src/components/ListLinks.tsx
@@ -5,10 +5,10 @@ import { FaChessBishop, FaGithubAlt, FaInstagram, FaYoutube } from 'react-icons/
 export function ListLink() {
   return (
     <VStack spacing={4} color='white' >
+      <LinkButton icon={<FaInstagram />} name='INSTAGRAM' href='https://instagram.com/jgsbispo' />
+      <LinkButton icon={<FaChessBishop />} name='WEBSITE' href='https://bispo.vercel.app/' />
+      <LinkButton icon={<FaGithubAlt />} name='GITHUB' href='https://github.com/jgbispo' />
       <LinkButton icon={<FaYoutube />} name='YOUTUBE' href='https://youtube.com' />
-      <LinkButton icon={<FaInstagram />} name='INSTAGRAM' href='https://youtube.com' />
-      <LinkButton icon={<FaChessBishop />} name='WEBSITE' href='https://youtube.com' />
-      <LinkButton icon={<FaGithubAlt />} name='GITHUB' href='https://youtube.com' />
       <Text color='brand.400'>Created by João Gustavo "Bishop"</Text>
     </VStack>
   )
